c3afeb3aa647983e9b05850595f6077166eab092
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2023-02-24 17:56:46 +0100

    feat: introduce state for count

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Show animal list here!</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
+    </div>
+  );
 }
 
 export default App;
